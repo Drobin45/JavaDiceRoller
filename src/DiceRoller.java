a73a06e017e84162f03490f96956e097e4c1ca00
Daniel <drobin897@gmail.com>
2017-11-26 15:02:59 -0500

    Created Die class with constructor and several methods. Implimented these methods in main program

--- src/DiceRoller.java
+++ src/DiceRoller.java
@@ -1,9 +1,23 @@
+import DiceRoller.Die;
 
 public class DiceRoller {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Die d12 = new Die(12);
+		Die d6 = new Die(6);
+		/*
+		System.out.println(d6.roll());
+		System.out.println(d12.roll());
+		
+		System.out.println(d6.getValue());
+		
+		d6.setSides(100);
+		System.out.println(d6.getSides());
+		System.out.println(d6.roll());
+		*/
+		
+		System.out.printf("Rolling a %d die. You rolled a %d! \n", d6.getSides(), d6.roll());
+		System.out.printf("Rolling a %d die. You rolled a %d!", d12.getSides(), d12.roll());
 	}
 
 }
--- src/DiceRoller/Die.java
+++ src/DiceRoller/Die.java
@@ -1,4 +1,52 @@
 package DiceRoller;
+
+import java.util.Random;
+
 public class Die {
 
+	//roll()
+	//getValue()
+	//getSides()
+	//setSides(int sides)
+	
+	private int sides;
+	private int value;
+	private Random generator;
+	
+	//Using this. makes the appended word point to the object of the constructor
+	// So, using this. in the below method will point to the above constructor
+	
+	public Die(int sides)
+	{
+	this.sides = sides;	
+	this.generator = new Random();
+	}
+	
+	//Overloaded constructor
+	public Die(int sides, int seed)
+	{
+		this.sides = sides;
+		this.generator = new Random(seed);
+	}
+	
+	public int roll()
+	{
+		this.value = this.generator.nextInt(this.sides) + 1;
+		return this.value;
+	}
+	
+	public int getValue()
+	{
+		return this.value;
+	}
+	
+	public int getSides()
+	{
+		return this.sides;
+	}
+	
+	public void setSides(int sides)
+	{
+		this.sides = sides;
+	}
 }
